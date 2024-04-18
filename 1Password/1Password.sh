11e4e5ca808b290680a9184e1feb5aeac395fa31
sis032 <i.serenko@truvity.com>
2024-04-18 18:32:10 +0500

    no message

--- 1Password/1Password.sh
+++ 1Password/1Password.sh
@@ -16,14 +16,9 @@ app_path="/Applications/1Password.app/Contents/Info.plist"
 # Check install
 if [ ! -f "$app_path" ]; then
 	cd /tmp/
-	# Download 1Password
-	curl -sL https://downloads.1password.com/mac/1Password.zip | tar xz
-	#if [ $? -gt 0 ]; then text_slack="Error downloading 1Password in $Company $(hostname)."; color='danger'; Slack_notification; exit 1; fi;
 	# Install 1Password
-	open /Volumes/1Password/1Password\ Installer.app
-	sleep 20
-	# Remove temp files
-	rm /tmp/1Password.dmg
+	brew install --cask 1password
+	if [ $? -gt 0 ]; then text_slack="Error of brew installing 1Password in $Company $(hostname)."; color='danger'; Slack_notification; exit 1; fi;
 	# Check install
 	if [ -f "$app_path" ]; then
 		text_slack="1Password is installed in $Company $(hostname)." 
--- Start-Intune-test.sh
+++ Start-Intune-test.sh
@@ -4,6 +4,6 @@ cd /tmp/
 curl -sL https://github.com/truvity/ITOps-Apple/archive/refs/heads/test.zip |  tar xz
 chmod -R a+x  ITOps-Apple-test/
 cd ITOps-Apple-test
-/bin/sh Listprogramuser.sh
+/bin/sh Listprogramuser.sh All
 sleep 20
 rm -rf /tmp/ITOps-Apple-test
