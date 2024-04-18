94d1a2bdaf6e753a9b88d29904bc62aa0e4b29c0
sis032 <i.serenko@truvity.com>
2024-04-18 13:59:58 +0500

    no message

--- Listprogramuser.sh
+++ Listprogramuser.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 set +e
 
+#Set name Company
+export Company="Truvity"
+
 #Set group
 Group="$1"
 
 #Set program
 Grammarly="On"
-
+Grammarly_Group=("All")
 
 
 if [ "$Grammarly" == "On" ]; then
-    /bin/sh Grammarly/Grammarly.sh
+	for val in "${Grammarly_Group[@]}"; do
+    if [ "$val" == "$Group" ]; then
+        /bin/sh Grammarly/Grammarly.sh
+
+    fi
+done
 fi
 
 
@@ -20,4 +28,3 @@ fi
 
 
 set -e
-rm -rf /tmp/ITOps-Apple-master
