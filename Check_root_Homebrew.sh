d0ebd3687732ab91016032cd685470fb05582ee1
sis032 <i.serenko@truvity.com>
2024-06-11 20:09:18 +0500

    Update Check_root_Homebrew.sh

--- Check_root_Homebrew.sh
+++ Check_root_Homebrew.sh
@@ -9,7 +9,7 @@ function Slack_notification() {
 ######
   local message="payload={\"attachments\":[{\"text\":\"$text_slack\",\"color\":\"$color\"}]}"
   curl -X POST --data-urlencode "$message" ${SLACK_WEBHOOK_URL}
-  
+  sleep 1
   curl --http1.1 -F file=@/tmp/detail_log.txt \
        -F "initial_comment=${text_slack}" \
        -F channels=${channel} \
