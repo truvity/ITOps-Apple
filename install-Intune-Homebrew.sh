aac7eb7324bd69d964cb30785975834f434c8775
sis032 <i.serenko@truvity.com>
2024-06-11 18:49:40 +0500

    delete files

--- install-Intune-Homebrew.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sed '/wait_for_user() {/,/^}/d')"
-sudo grep -q '%admin ALL=(ALL) NOPASSWD:SETENV: /opt/homebrew/*/* *, /usr/sbin/installer -pkg /opt/homebrew/*, /bin/launchctl list *' /etc/sudoers || echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' | sudo tee -a /etc/zprofile > /dev/null
-sudo chown -R :admin /opt/homebrew
-sudo chmod -R 775 /opt/homebrew
-sudo grep -q 'eval "\$(/opt/homebrew/bin/brew shellenv)"' /etc/zprofile || echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' | sudo tee -a /etc/zprofile > /dev/null
-source /etc/zprofile
-brew install --cask intune-company-portal
-open /Applications/Company\ Portal.app
