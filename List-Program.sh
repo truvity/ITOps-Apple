454df3a23593e8c38ac9aa5cfe1a4b70a8ffecb1
sis032 <i.serenko@truvity.com>
2024-07-24 17:01:24 +0500

    Update List-Program.sh

--- List-Program.sh
+++ List-Program.sh
@@ -30,6 +30,9 @@ if [ "$Company" = "Truvity" ]; then
 			google-drive
 			anydesk
 			whatsapp
+			applite
+			iterm2
+			microsoft-remote-desktop
 		# Add other programs here
 		)
 		
@@ -59,6 +62,9 @@ if [ "$Company" = "Finerbase" ]; then
 			google-drive
 			anydesk
 			whatsapp
+			applite
+			iterm2
+			microsoft-remote-desktop
 		# Add other programs here
 		)
 		
@@ -94,6 +100,9 @@ if [ "$Company" = "Datagrid" ]; then
 			zoom
 			google-drive
 			anydesk
+			applite
+			iterm2
+			microsoft-remote-desktop
 		# Add other programs here
 		)
 		
